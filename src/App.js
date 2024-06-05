90a0111f2d47de2a79a211fe54fa21166b4d83d0
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-06-05 19:47:48 +0200

    Container Input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./PaginaDeLogin.css";
+import "./CampoDeDigitacao.css";
+import ImageLogin from "./img/imagem-login.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Componente Titulo
+    function Titulo() {
+        return <h1 className="form__titulo">Login</h1>;
+    }
+    // Componente Subtitulo
+    function Subtitulo() {
+        return <h3 className="form__texto">Boas-vindas! Faça seu login.</h3>;
+    }
+    // Componente Campo de Digitação
+    function CampoDeDigitacao({ label, type, placeholder }) {
+        return (
+            <div className="form__campo-digitacao">
+                <label for="email">{label}</label>
+                <input
+                    type={type}
+                    placeholder={placeholder}
+                    required
+                    id="email"
+                />
+            </div>
+        );
+    }
+    // Componente Pagina de Login
+    function PaginaDeLogin() {
+        return (
+            <div className="container-login">
+                <img
+                    src={ImageLogin}
+                    alt="Garota Negra no computador com oculos e cabelo cacheado"
+                />
+                <section>
+                    <Titulo />
+                    <Subtitulo />
+                    <CampoDeDigitacao
+                        label="Email ou Usuário"
+                        type="email"
+                        placeholder="Digite seu Email."
+                    />
+                </section>
+            </div>
+        );
+    }
+
+    return (
+        <div className="App">
+            <PaginaDeLogin />
+        </div>
+    );
 }
 
 export default App;
