50c4572be8fa2916903284a5098f2a5591093292
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-06-06 19:49:47 +0200

    Adicionando State para usuário e senha

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "./PaginaDeLogin.css";
 import "./CampoDeDigitacao.css";
 import ImageLogin from "./img/imagem-login.png";
+import { useState } from "react";
 
 function App() {
     // Componente Titulo
@@ -13,21 +14,36 @@ function App() {
         return <h3 className="form__texto">Boas-vindas! Faça seu login.</h3>;
     }
     // Componente Campo de Digitação
-    function CampoDeDigitacao({ label, type, placeholder }) {
+    function CampoDeDigitacao({ label, type, placeholder, value, setValue }) {
         return (
             <div className="form__campo-digitacao">
-                <label for="email">{label}</label>
+                <label for={type}>{label}</label>
                 <input
                     type={type}
                     placeholder={placeholder}
                     required
-                    id="email"
+                    id={type}
+                    value={value}
+                    onChange={(event) => setValue(event.target.value)}
                 />
             </div>
         );
     }
+    // Componente Button
+    function Botao() {
+        return <button className="form__botao">Login</button>;
+    }
     // Componente Pagina de Login
     function PaginaDeLogin() {
+        // useState de Email e Senha
+        const [email, setEmail] = useState("");
+        const [senha, setSenha] = useState("");
+        // Evento de handleSubmit aonde pega o valor e salva.
+        const handleSubmit = (event) => {
+            event.preventDefault();
+            console.log("Email " + email);
+            console.log("Senha " + senha);
+        };
         return (
             <div className="container-login">
                 <img
@@ -35,13 +51,25 @@ function App() {
                     alt="Garota Negra no computador com oculos e cabelo cacheado"
                 />
                 <section>
-                    <Titulo />
-                    <Subtitulo />
-                    <CampoDeDigitacao
-                        label="Email ou Usuário"
-                        type="email"
-                        placeholder="Digite seu Email."
-                    />
+                    <form onSubmit={handleSubmit}>
+                        <Titulo />
+                        <Subtitulo />
+                        <CampoDeDigitacao
+                            label="Email ou Usuário"
+                            type="email"
+                            placeholder="Digite seu Email."
+                            value={email}
+                            setValue={setEmail}
+                        />
+                        <CampoDeDigitacao
+                            label="Senha"
+                            type="password"
+                            placeholder="*****"
+                            value={senha}
+                            setValue={setSenha}
+                        />
+                        <Botao />
+                    </form>
                 </section>
             </div>
         );
